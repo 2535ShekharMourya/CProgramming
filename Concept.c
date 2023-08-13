d8a140592ea032fc858360fc2cc75efbd62543bb
Azadshekhar <shekharmaurya8978@gmail.com>
2023-08-13 21:48:12 +0530

    first commit

---
+++ Concept.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+   int i=3;
+   int j=5;
+   if(j!=3){
+    printf("sai hai");
+   }
+    
+}
---
+++ PracticProgram1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+ int i,j;
+ scanf("%d",&i);
+ scanf("%d",&j);
+ printf(" the sum is : %d\n",i+j);
+ return 0;
+}
